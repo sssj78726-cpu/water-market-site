35d98de849b71567cd13b6be9ae1e4ef1cf978df
indominis rexyyy <lesa29504@gmail.com>
2026-04-30 10:53:12 +0300

    first commit

---
+++ add.py
@@ -0,0 +1,13 @@
+from app import db, Product, app
+
+with app.app_context():
+    if Product.query.count() == 0:
+        products = [
+            Product(name='water 1L', price=1, image='water1.png'),
+            Product(name='water 5L', price=5, image='water2.png')
+        ]
+        db.session.add_all(products)
+        db.session.commit()
+        print("pop")
+    else:
+        print("not pop")
---
+++ app.py
@@ -0,0 +1,166 @@
+from flask import Flask,render_template,request,url_for,session,redirect
+import hashlib
+from dotenv import load_dotenv
+import os
+from flask_wtf import FlaskForm
+from wtforms import DateTimeLocalField, StringField, SelectField, SubmitField,PasswordField
+from wtforms.validators import DataRequired, Length
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from sqlalchemy.exc import IntegrityError
+
+
+load_dotenv()
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///marcet.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    login = db.Column(db.String(80), unique = True, nullable = False)
+    Password = db.Column(db.String(120), nullable = False)
+    is_admin = db.Column(db.Integer, default = 0)
+
+class Cartss(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    product_id = db.Column(db.Integer,nullable = False)
+    user_id = db.Column(db.Integer, nullable = False)
+    quantity = db.Column(db.Integer)
+
+class Product(db.Model):
+    id = db.Column(db.Integer,primary_key = True)
+    name = db.Column(db.String(15), unique = True, nullable = False)
+    price = db.Column(db.Integer)
+    image = db.Column(db.String(150), nullable = False)
+
+app.secret_key = os.getenv('SECRET_KEY')
+
+class RegForm(FlaskForm):
+    login = StringField('login',validators=[DataRequired(),Length(min=4, message='min lenght 4 simvols!')])
+    password = PasswordField('password', validators=[DataRequired(),Length(min=6,message='min lenght 6 simvols!')])
+    submit = SubmitField('registration')
+
+class LoginForm(FlaskForm):
+    login = StringField('login',validators=[DataRequired()])
+    password = PasswordField('password',validators=[DataRequired()])
+    submit = SubmitField('sign in')
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/registration',methods=['GET','POST'])
+def reg():
+    form = RegForm()
+    try:
+        if request.method == "POST" and form.validate_on_submit():
+            login = form.login.data
+            password = form.password.data
+            hashs = hashlib.sha256(password.encode()).hexdigest()
+            is_admin = 0
+            new_user = User(login = login, Password = hashs, is_admin = is_admin)
+            db.session.add(new_user)
+            db.session.commit()
+            return "<h1>you registr</h1><a href='/login'>sign in</a>"
+        else:
+            return render_template('reg.html',form = form)
+    except IntegrityError:
+        db.session.rollback()
+        return '<h1>login in base</h1><a href="/">home</a>'
+    
+@app.route('/login',methods =["GET","POST"])
+def login():
+    form = LoginForm()
+    try:
+        if request.method == "POST" and form.validate_on_submit():
+            login = form.login.data
+            password = form.password.data
+            hashs = hashlib.sha256(password.encode()).hexdigest()
+            user = User.query.filter_by(login = login, Password = hashs).first()
+            if user:
+                session['user'] = user.login
+                return redirect(url_for('home'))
+            else:
+                return '<h1>bad password or login</h1><a href"/">home</a>'
+        else:
+            return render_template('login.html',form = form)
+    except Exception as e:
+        return str(e)
+
+@app.route('/catalog')
+def cataloge():
+    products = Product.query.all()
+    if products:
+        return render_template('cataloge.html',products = products )
+
+@app.route('/add_cart/<int:product_id>')
+def add_cart(product_id):
+    if 'user' not in session:
+        return redirect(url_for('home'))
+    user = User.query.filter_by(login = session['user']).first()
+    if not user:
+        return redirect(url_for('login'))
+    cart_item = Cartss.query.filter_by(user_id =user.id,product_id = product_id).first()
+    if cart_item:
+        cart_item.quantity += 1
+        db.session.add(cart_item)
+        db.session.commit()
+        return redirect(url_for('cataloge'))
+    else:
+        cart_item = Cartss(user_id =user.id,product_id = product_id, quantity = 1)
+        db.session.add(cart_item)
+        db.session.commit()
+        return redirect(url_for('cataloge'))
+    
+@app.route('/cart')
+def cart():
+    if 'user' not in session:
+        return redirect(url_for('login'))
+    user = User.query.filter_by(login = session['user']).first()
+    if not user:
+        return redirect(url_for('login'))
+    user_id = User.query.filter_by(login = session['user']).first()
+    items = db.session.query(Product.name, Product.price, Product.image, Cartss.quantity, Product.id).join( Cartss, Product.id == Cartss.product_id).filter(Cartss.user_id == User.id).all()
+    return render_template('cart.html', items=items)
+
+@app.route('/remove_cart/<int:product_id>')
+def remove_cart(product_id):
+    if 'user' not in session:
+        return redirect(url_for('home'))
+    user = User.query.filter_by(login = session['user']).first()
+    if not user:
+        return redirect(url_for('home'))
+    result = Cartss.query.filter_by(user_id = user.id, product_id = product_id).first()
+    if result:
+        d = Cartss.query.filter_by(user_id = user.id,product_id = product_id).delete()
+        db.session.commit()
+        return redirect(url_for('cart'))
+    else:
+        return redirect(url_for('cart'))
+
+@app.route('/delete_one/<int:product_id>')
+def delete_one(product_id):
+    if 'user' not in session:
+        return redirect(url_for('home'))
+    user = User.query.filter_by(login = session['user']).first()
+    if not user:
+        return redirect(url_for('home'))
+    q = Cartss.query.filter_by(user_id = user.id, product_id = product_id).first()
+    if q:
+        if q.quantity > 0:
+            q.quantity -=1
+            db.session.add(q)
+            db.session.commit()
+            return redirect(url_for('cart'))
+        else:
+            d = Cartss.query.filter_by(user_id = user.id,product_id = product_id).delete()
+            db.session.commit()
+            return redirect(url_for('cart'))
+
+
+if __name__ == '__main__':
+    app.run(debug= True)
+    
